e68630a67cac5d3a5fd63850b262c94d76384c00
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-07-16 23:18:41 +0900

    fix(clear_data.c): fix parameter of pthread_mutex_destroy

--- philo/srcs/clear_data.c
+++ philo/srcs/clear_data.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 09:24:32 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/07/08 09:34:06 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/07/16 23:16:48 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	clear_data(t_data *data)
 	int		i;
 	t_philo	*tmp;
 
-	pthread_mutex_destroy(&m_flag);
-	pthread_mutex_destroy(&m_total_finished);
-	pthread_mutex_destroy(&m_write);
+	pthread_mutex_destroy(&data->m_flag);
+	pthread_mutex_destroy(&data->m_total_finished);
+	pthread_mutex_destroy(&data->m_write);
 	i = 0;
 	tmp = data->philo;
 	while (i++ < data->total)
